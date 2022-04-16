803378e138f451a6e9493983923726992e646b5c
huangjunjie <huangjunjie@bilibili.com>
2022-04-16 13:33:20 +0800

    modify JumpGameII

--- src/main/java/me/junjiehuang1020/homework/week/seven/JumpGameII.java
+++ src/main/java/me/junjiehuang1020/homework/week/seven/JumpGameII.java
@@ -2,23 +2,25 @@ package me.junjiehuang1020.homework.week.seven;
 
 public class JumpGameII {
     
-    int[] dp = new int[nums.length];
-
+    public int jump(int[] nums) {
+        
+        int[] dp = new int[nums.length];
+        
         for (int i = 0; i < nums.length; i++) {
-        dp[i] = Integer.MAX_VALUE - 1;
-    }
-    
-    dp[0] = 0;
-
+            dp[i] = Integer.MAX_VALUE - 1;
+        }
+        
+        dp[0] = 0;
+        
         for (int i = 0; i < nums.length; i++) {
-        for (int j = 0; j < i; j++) {
-            if (j + nums[j] >= i) {
-                dp[i] = Math.min(dp[i], dp[j] + 1);
+            for (int j = 0; j < i; j++) {
+                if (j + nums[j] >= i) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
             }
         }
-    }
-
+        
         return dp[nums.length - 1];
-    
-}
+        
+    }
 }
